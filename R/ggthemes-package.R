ae7b68faeb56b4951804c3d50afeb87b19b6fdad
Jeffrey B. Arnold <jeffrey.arnold@gmail.com>
2015-07-01 11:30:07 +0000

    version 2.2.1

--- R/ggthemes-package.R
+++ R/ggthemes-package.R
@@ -1,12 +1,20 @@
-##' ggthemes
-##'
-##' This package contains extra themes, scales, and geoms, and
-##' functions for and related to \pkg{ggplot2}.
-##'
-##' In addition to the help pages, see the README page on
-##' \href{https://github.com/jrnold/ggthemes}{github} for examples.
-##'
-##' @name ggthemes
-##' @docType package
-##' @import colorspace grid proto scales ggplot2
+#' ggthemes
+#'
+#' This package contains extra themes, scales, and geoms, and
+#' functions for and related to \pkg{ggplot2}.
+#'
+#' In addition to the help pages, see the README page on
+#' \href{https://github.com/jrnold/ggthemes}{github} for examples.
+#'
+#' @name ggthemes
+#' @docType package
+#' @import stats
+#' @import utils
+#' @import colorspace
+#' @import grid
+#' @import proto
+#' @import scales
+#' @import ggplot2
+#' @importFrom graphics abline axis text points
+#' @importFrom methods hasArg as
 NULL
