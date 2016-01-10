76ab6a33ab8c4699f1d11d1a6efb231031b4df5d
Jeffrey B. Arnold <jeffrey.arnold@gmail.com>
2016-01-10 09:08:58 +0000

    version 3.0.1

--- R/excel.R
+++ R/excel.R
@@ -69,18 +69,25 @@ scale_color_excel <- scale_colour_excel
 #'      geom_point(aes(x = wt, y = mpg, colour=factor(gear))) +
 #'      facet_wrap(~am)
 #' p + theme_excel() + scale_colour_excel()
-#' 
+#'
 #' # Old fill color palette
-#' (ggplot(diamonds, aes(clarity, fill=cut))
-#' + geom_bar()
-#' + scale_fill_excel('fill')
-#' + theme_excel())
+#' ggplot(mpg, aes(x = class, fill = drv)) +
+#'   geom_bar() +
+#'   scale_fill_excel('fill') +
+#'   theme_excel()
 #'
 theme_excel <- function(base_size = 12, base_family = "", horizontal = TRUE) {
   gray <- "#C0C0C0"
-  ret <- (theme_bw() + theme(panel.background = element_rect(fill = gray), panel.border = element_rect(colour = "black", 
-    linetype = 1), panel.grid.major = element_line(colour = "black"), panel.grid.minor = element_blank(), legend.key = element_rect(colour = NA), 
-    legend.background = element_rect(colour = "black", linetype = 1), strip.background = element_rect(fill = "white", 
+  ret <- (theme_bw() +
+            theme(panel.background = element_rect(fill = gray),
+                  panel.border = element_rect(colour = "black",
+                                              linetype = 1),
+                  panel.grid.major = element_line(colour = "black"),
+                  panel.grid.minor = element_blank(),
+                  legend.key = element_rect(colour = NA),
+
+    legend.background = element_rect(colour = "black", linetype = 1),
+ strip.background = element_rect(fill = "white",
       colour = NA, linetype = 0)))
   if (horizontal) {
     ret <- ret + theme(panel.grid.major.x = element_blank())
@@ -88,4 +95,4 @@ theme_excel <- function(base_size = 12, base_family = "", horizontal = TRUE) {
     ret <- ret + theme(panel.grid.major.y = element_blank())
   }
   ret
-} 
+}
